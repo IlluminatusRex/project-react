9fe58a63088b4882fd1abbca8eea6f85572b9a4d
Jakub Czesak <jakub.czesak@cbre.com>
2023-04-12 22:40:33 +0200

    Add filter

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,21 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 const SearchForm = () => {
+    const [searchString, setTitle] = useState('');
+    const dispatch = useDispatch();
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({ type: 'UPDATE_SEARCHSTRING', payload: searchString });
+        setTitle('');
+    };
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search…" />
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+            <TextInput type="text" placeholder="Search…" value={searchString} onChange={e => setTitle(e.target.value)}/>
             <Button>Search…</Button>
         </form>
     );
