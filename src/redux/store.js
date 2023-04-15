5744bc5703a7d15f3dd69ff4ed506970e641e16d
Jakub Czesak <jakub.czesak@cbre.com>
2023-04-15 15:40:10 +0200

    Fix store.js export addCart

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,7 @@ export const getFilteredCards = ({ cards, searchString }, columnId) => cards
 export const getAllColumns = state => state.columns;
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
-export const addCart = payload => ({ type: 'ADD_CART', payload });
+export const addCart = payload => ({ type: 'ADD_CARD', payload });
 export const updateSearch = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
 
 const reducer = (state, action) => {
